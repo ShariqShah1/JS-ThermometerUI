1db9184d995b4f439fb73f4221586eab9005128c
Shariq Shah <shahshariq47@gmail.com>
2023-05-12 18:34:49 +0500

    first commit

---
+++ app.js
@@ -0,0 +1,26 @@
+const tempload= ()=>{
+    let temp=document.getElementById("thermo");
+    temp.innerHTML= "&#xf2cb";
+
+    setTimeout(()=>{
+        temp.innerHTML="&#xf2ca";
+        temp.style.color="yellow"
+    },1000)
+
+    setTimeout(()=>{
+        temp.innerHTML="&#xf2c9";
+        temp.style.color="yellow"
+    },2000)
+
+    setTimeout(()=>{
+        temp.innerHTML="&#xf2c8";
+        temp.style.color="yellow"
+    },3000)
+
+    setTimeout(()=>{
+        temp.innerHTML="&#xf2c7";
+        temp.style.color="red"
+    },4000)
+}
+tempload();
+setInterval(tempload,5000);
